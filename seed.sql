edb403db0a187a2a7fa9cb74bca09c10bb7ab5ac
Dylan Cole <logboarddjc@gmail.com>
2021-05-25 16:27:03 -0400

    add foreign keys to table in seed.sql

--- seed.sql
+++ seed.sql
@@ -3,32 +3,34 @@ CREATE database businessDB;
 
 USE businessDB;
 
-CREATE TABLE employee (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT NULL
-);
-
-CREATE TABLE role (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary VARCHAR(30),
-    department_id INT NOT NULL
-);
-
 CREATE TABLE department (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(30)
 );  
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+CREATE TABLE role (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary VARCHAR(30),
+    department_id INT, 
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id int, 
+    manager_id int, 
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
+INSERT INTO department (name)
 VALUES 
-("Thomas", "Smith", 3, NULL), 
-("Tom", "Smath", 1, 1), 
-("Tim", "Smerth", 1, 1), 
-("Sam", "Smeth", 2, 1);
+("engineering"),
+("sales"), 
+("management");
 
 INSERT INTO role (title, salary, department_id)
 VALUES 
@@ -36,11 +38,12 @@ VALUES
 ("sales", 95000, 2), 
 ("manager", 150000, 3);
 
-INSERT INTO department (name)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-("engineering"),
-("sales"), 
-("management");
+("Thomas", "Smith", 3, NULL), 
+("Tom", "Smath", 1, 1), 
+("Tim", "Smerth", 1, 1), 
+("Sam", "Smeth", 2, 1);
 
 SELECT * FROM employee;
 SELECT * FROM role;
