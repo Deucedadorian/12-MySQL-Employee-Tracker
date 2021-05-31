e4a5257572160d45b0e4c25b15ad1cfac4a4c004
Dylan Cole <logboarddjc@gmail.com>
2021-05-30 21:23:34 -0400

    still working on add Employee Function

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,16 +1,16 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
-const cTable = require('console.table');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 // const { viewEmployees } = require("./lib/Employee");
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
 
   port: 3306,
-  
-  user: 'root',
-  password: '12345',
-  database: 'businessDB',
+
+  user: "root",
+  password: "12345",
+  database: "businessDB",
 });
 
 connection.connect((err) => {
@@ -18,27 +18,27 @@ connection.connect((err) => {
   SelectTask();
 });
 
-const SelectTask = () => {
+let SelectTask = () => {
   inquirer
     .prompt({
-      name: 'action',
-      type: 'list',
-      message: 'What would you like to do?',
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
       choices: [
-        'View All Employees',
-        'View All Employees By Department',
-        'View All Employees By Manager',
-        'Add New Employee',
-        'Remove Employee',
-        'Update Employee Role',
-        'Update Employee Manager',
-        'View All Roles',
-        'Add New Role',
-        'Remove Role',
-        'View All Departments',
-        'Add New Department',
-        'Remove Department',
-        'View Total Utilized Budget of Department',
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add New Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "View All Roles",
+        "Add New Role",
+        "Remove Role",
+        "View All Departments",
+        "Add New Department",
+        "Remove Department",
+        "View Total Utilized Budget of Department",
       ],
     })
     .then((answer) => {
@@ -106,7 +106,7 @@ const SelectTask = () => {
     });
 };
 
-const viewEmployees = () => {
+let viewEmployees = () => {
   const query = `
   SELECT 
 	e.id, 
@@ -117,17 +117,119 @@ const viewEmployees = () => {
 	salary,
     m.first_name AS manager
 FROM employees e
-JOIN roles r 
+LEFT JOIN roles r 
 	ON e.role_id = r.id
 LEFT JOIN employees m
 	ON e.manager_id = m.id
-JOIN departments d
-	ON r.id = d.id
-ORDER BY e.id`;
+LEFT JOIN departments d
+	ON r.id = d.id`;
   connection.query(query, (err, res) => {
     console.table(res);
     SelectTask();
   });
 };
 
-module.exports = connection;
+let addEmployee = () => {
+  inquirer
+  .prompt([
+    {
+      name: "firstName",
+      type: "input",
+      message: "Enter the employee's first name:",
+    },
+    {
+      name: "lastName",
+      type: "input",
+      message: "Enter the employee's last name:",
+    },
+    {
+      name: "role",
+      type: "list",
+      message: "Enter the employee's role:",
+      choices() {
+        connection.query(
+          `SELECT
+            title
+          FROM roles`, 
+          (err, res) => {
+              if (err) throw err;
+            const choiceArray = [];
+            res.forEach(({ title }) => {
+              choiceArray.push(title);
+            });
+            return choiceArray;
+          },
+        );
+      },
+    },
+    {
+      name: "manager",
+      type: "list",
+      choices() {
+        connection.query(
+          `SELECT
+            first_name,
+            last_name
+          FROM employees
+          WHERE role_id = 3`, 
+          (err, res) => {
+            if (err) throw err;
+            const choiceArray = [];
+            res.forEach(({ first_name, last_name }) => {
+              choiceArray.push(first_name + " " + last_name);
+            });
+            return choiceArray;
+          }
+        );
+      },
+    },
+  ])
+  .then((answers) => {
+    connection.query(
+      "INSERT INTO employees SET ?",
+      {
+        first_name: answers.firstName,
+        last_name: answers.lastName,
+        role_id: getRoleId(),
+        manager_id: managerId()
+      },
+      (err) => {
+        if (err) throw err;
+        SelectTask();
+      }
+    );
+  })
+};
+
+const getRoleId = () => {
+  connection.query('SELECT id FROM roles WHERE ?', 
+    {
+      title: answers.role,
+    },
+    (err, res) => {
+      if (err) throw err;
+      let roleId = [];
+      res.forEach(({ id }) => {
+        roleId.push(id);
+      });
+      return roleId; 
+    },
+  );
+}
+
+let managerId = () => {
+  let manager = answers.manager.split(' ');
+  connection.query(
+    'SELECT id FROM employees WHERE ?',
+    {
+      first_name: manager[0],
+      last_name: manager[1],
+    },
+    (err, res) => {
+      if (err) throw err;
+      let managerId;
+        managerId = res.id;
+      return managerId;
+    },
+  );
+}
