f6e1b7a9db60ca48cc4cf34b11d93f9c5bfd271d
Dylan Cole <logboarddjc@gmail.com>
2021-06-01 16:02:46 -0400

    add working functions for viewing and adding departments, roles and employees and for updating employees

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,104 +1,90 @@
 const util = require('util');
-const mysql = require("mysql");
-const inquirer = require("inquirer");
-const cTable = require("console.table");
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 const { promisify } = require('util');
-
-const connection = mysql.createConnection({
-  host: "localhost",
-
-  port: 3306,
-
-  user: "root",
-  password: "12345",
-  database: "businessDB",
-});
+const { connection } = require('./connection');
+const { type } = require('os');
 
 const queryAsync = util.promisify(connection.query).bind(connection);
 
 connection.connect((err) => {
   if (err) throw err;
-  SelectTask();
+  selectTask();
 });
 
-let SelectTask = async () => {
+let selectTask = () => {
   inquirer
     .prompt({
-      name: "action",
-      type: "list",
-      message: "What would you like to do?",
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
       choices: [
-        "View All Employees",
-        "View All Employees By Department",
-        "View All Employees By Manager",
-        "Add New Employee",
-        "Remove Employee",
-        "Update Employee Role",
-        "Update Employee Manager",
-        "View All Roles",
-        "Add New Role",
-        "Remove Role",
-        "View All Departments",
-        "Add New Department",
-        "Remove Department",
-        "View Total Utilized Budget of Department",
+        'View All Employees',
+        // 'View All Employees By Department',
+        // 'View All Employees By Manager',
+        // 'View All Roles',
+        // 'View All Departments',
+        'View Total Utilized Budget of Department',
+        // 'Add New Employee',
+        // 'Add New Role',
+        // 'Add New Department',
+        // 'Update Employee',
+        'Remove Employee',
+        'Remove Role',
+        'Remove Department',
       ],
     })
     .then((answer) => {
       switch (answer.action) {
-        case "View All Employees":
+        case 'View All Employees':
           viewEmployees();
           break;
 
-        case "View All Employees By Department":
+        case 'View All Employees By Department':
           employeeByDepartment();
           break;
 
-        case "View All Employees By Manager":
+        case 'View All Employees By Manager':
           employeesByManager();
           break;
 
-        case "Add New Employee":
+        case 'Add New Employee':
           addEmployee();
           break;
 
-        case "Remove Employee":
+        case 'Remove Employee':
           removeEmployee();
           break;
 
-        case "Update Employee Role":
-          updateRole();
+        case 'Update Employee':
+          updateEmpolyee();
           break;
 
-        case "Update Employee Manager":
-          updateManager();
-          break;
-
-        case "View All Roles":
+        case 'View All Roles':
           viewRoles();
           break;
 
-        case "Add New Role":
+        case 'Add New Role':
           addRole();
           break;
 
-        case "Remove Role":
+        case 'Remove Role':
           removeRole();
           break;
 
-        case "View All Departments":
+        case 'View All Departments':
           viewDepartments();
           break;
 
-        case "Add New Department":
+        case 'Add New Department':
           addDepartment();
           break;
 
-        case "Remove Department":
+        case 'Remove Department':
           removeDepartment();
           break;
 
-        case "View Total Utilized Budget of Department":
+        case 'View Total Utilized Budget of Department':
           departmentBudget();
           break;
 
@@ -109,77 +95,120 @@ let SelectTask = async () => {
     });
 };
 
-// let viewEmployees = () => {
-//   const query = `
-//   SELECT 
-// 	e.id, 
-//     e.first_name, 
-//     e.last_name, 
-//     title, 
-// 	d.name AS department,
-// 	salary,
-//     m.first_name AS manager
-// FROM employees e
-// LEFT JOIN roles r 
-// 	ON e.role_id = r.id
-// LEFT JOIN employees m
-// 	ON e.manager_id = m.id
-// LEFT JOIN departments d
-// 	ON r.id = d.id`;
-//   connection.query(query, (err, res) => {
-//     console.table(res);
-//     SelectTask();
-//   });
-// };
+// View Departments, Roles and Employees
+let viewEmployees = () => {
+  const query = `
+  SELECT 
+    e.id, 
+    e.first_name, 
+    e.last_name, 
+    title, 
+    d.name AS department,
+    salary,
+    m.first_name AS manager
+FROM employees e
+LEFT JOIN roles r 
+    ON e.role_id = r.id
+LEFT JOIN employees m
+    ON e.manager_id = m.id
+LEFT JOIN departments d
+    ON r.id = d.id`;
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    selectTask();
+  });
+};
 
-const addEmployee = () => {
+let viewDepartments = () => {
+  connection.query(`SELECT name AS Departments FROM departments;`,
+  (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    selectTask();
+  });
+}
+
+let viewRoles = () => {
+  connection.query('SELECT title AS Roles FROM roles;',
+  (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    selectTask();
+  });
+} 
+
+// add Departments, Roles and Employees
+let addEmployee = () => {
   Promise.all([getRoles(), getManagers()])
-    .then(([ roles, managers ]) =>
-    // maybe here is the only place for a query.. So Ill have to make the right one.
-      inquirer
+  .then(([ roles, managers ]) =>
+    inquirer
     .prompt([
       {
-        name: "firstName",
-        type: "input",
-        message: "Enter the employee's first name:",
+        name: 'firstName',
+        type: 'input',
+        message: 'Enter the employee\'s first name:',
       },
       {
-        name: "lastName",
-        type: "input",
-        message: "Enter the employee's last name:",
+        name: 'lastName',
+        type: 'input',
+        message: 'Enter the employee\'s last name:',
       },
       {
-        name: "role",
-        type: "list",
-        message: "Enter the employee's role:",
+        name: 'role',
+        type: 'list',
+        message: 'Enter the employee\'s role:',
         choices: roles,
       },
       {
-        name: "manager",
-        type: "list",
-        choices: managers,
+        type: 'list',
+        name: 'managerCheck',
+        message: 'Does the employee have a manager?',
+        choices: ['yes', 'no'],
       },
-    ]))
-    .then((answers) => {
+      {
+        name: 'manager',
+        type: 'list',
+        choices: managers,
+        when: (answers) => answers.managerCheck === 'yes',
+      },
+    ])
+  )
+  .then((answers) => {
+    if (answers.managerCheck === 'yes') {
       connection.query(
-        "INSERT INTO employees SET ?",
+        'INSERT INTO employees SET ?',
         {
           first_name: answers.firstName,
           last_name: answers.lastName,
           role_id: Number(answers.role),
-          manager_id: Number(answers.manager)
+          manager_id: Number(answers.manager),
         },
         (err) => {
           if (err) throw err;
-          SelectTask();
+          selectTask();
         }
       );
-    })
+    } else {
+      connection.query(
+        'INSERT INTO employees SET ?',
+        {
+          first_name: answers.firstName,
+          last_name: answers.lastName,
+          role_id: Number(answers.role),
+        },
+        (err) => {
+          if (err) throw err;
+          selectTask();
+        }
+      );
+    }
+  })
 };
 
 let getRoles = async () => {
   try {
-    const rows = await queryAsync("SELECT * FROM roles");
+    const rows = await queryAsync('SELECT * FROM roles');
     return rows.map((role) => ({name: role.title, value: role.id}));
   } catch (err) {
       console.log(`Err at getRoles,`, err);
@@ -188,9 +217,151 @@ let getRoles = async () => {
 
 let getManagers = async () => {
   try {
-    const rows = await queryAsync("SELECT first_name, last_name, id FROM employees WHERE role_id = 3");
+    const rows = await queryAsync('SELECT first_name, last_name, id FROM employees WHERE manager_id IS NULL');
     return rows.map((manager) => ({name: `${manager.first_name} ${manager.last_name}`, value: manager.id}));
   } catch (err) {
       console.log(`Err at getRoles,`, err);
   }
 };
+
+let addDepartment = () => {
+  inquirer
+  .prompt({
+    name: 'department',
+    type: 'input',
+    message: 'Enter the name of the new department:'
+  })
+  .then((answers) => {
+    connection.query('INSERT INTO departments SET ?',
+      {
+        name: answers.department
+      },
+      (err) => {
+        if (err) throw err;
+        selectTask();
+      }
+    );
+  });
+}
+
+let addRole = async () => {
+  const departments = await getDepartments();
+  const answers = await inquirer.prompt([
+    {
+    name: 'title',
+    type: 'input',
+    message: 'Enter the name of the new role:'
+    },
+    {
+      name: 'salary',
+      type: 'number',
+      message: 'Enter the salary of the new role:'
+    },
+    {
+      name: 'departmentId',
+      type: 'list',
+      message: 'Enter the department id of the new role:',
+      choices: departments,
+    }
+  ])
+  await connection.query('INSERT INTO roles SET ?',
+    {
+      title: answers.title,
+      salary: answers.salary,
+      department_id: Number(answers.departmentId)
+    },
+    (err) => {
+      if (err) throw err;
+      selectTask();
+    }
+  );
+}
+
+let getDepartments = async () => {
+  try {
+    const rows = await queryAsync('SELECT * FROM departments');
+    return rows.map((department) => ({name: department.name, value: department.id}));
+  } catch (err) {
+      console.log(`Err at getDepartments,`, err);
+  }
+}
+
+// update Employee roles
+let updateEmpolyee = () => {
+  Promise.all([getEmployees(), getRoles(), getManagers()])
+  .then(([ employees, roles, managers ]) =>
+    inquirer
+    .prompt([
+      {
+        name: 'employee',
+        type: 'list',
+        message: 'Choose the employee to update:',
+        choices: employees,
+      },
+      {
+        name: 'role',
+        type: 'list',
+        messages: 'Choose the employee\'s role:',
+        choices: roles,
+      },
+      {
+        name: 'managerCheck',
+        type: 'list',
+        message: 'Does the employee now have a manager?',
+        choices: ['yes', 'no'],
+      },
+      {
+        name: 'manager',
+        type: 'list',
+        choices: managers,
+        message: 'Who is the employee\'s manager?',
+        when: (answers) => answers.managerCheck === 'yes',
+      },
+    ])
+  )
+  .then((answers) => {
+    if (answers.managerCheck === 'yes') {
+      connection.query(
+        'UPDATE employees SET ? WHERE ?',
+        [
+          {
+            role_id: answers.role,
+            manager_id: Number(answers.manager), 
+          },
+          {
+            id: Number(answers.employee)
+          }
+        ],
+        (err) => {
+          if (err) throw err;
+          selectTask();
+        }
+      );
+    } else {
+      connection.query('UPDATE employees SET ? WHERE ?',
+        [
+          {
+            role_id: answers.role,
+            manager_id: null, 
+          },
+          {
+            id: Number(answers.employee)
+          }
+        ],
+        (err) => {
+          if (err) throw err;
+          selectTask();
+        }
+      );
+    }
+  });
+}
+
+let getEmployees = async () => {
+  try {
+    const rows = await queryAsync('SELECT * FROM employees');
+    return rows.map((employee) => ({ name: `${employee.first_name} ${employee.last_name}`, value: employee.id}));
+  } catch (err) {
+    console.log('Err at getEmployees');
+  }
+}
